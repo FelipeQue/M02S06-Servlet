a8f9813ae10fbdf1c3b48a64a73e16af6a205598
Felipe Quérette <felipe.querette@gmail.com>
2024-06-24 14:11:13 -0300

    changed annotation syntax

--- src/main/java/petshop/TutoriaServlet.java
+++ src/main/java/petshop/TutoriaServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/tutoria/*")
+@WebServlet(value = "/tutoria")
 class TutoriaServlet extends HttpServlet {
 	
 	@Override
