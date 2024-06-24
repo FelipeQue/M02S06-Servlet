635bad81a3daf5762bb3f2eb8e9bcb8cbdb74b86
Felipe Quérette <felipe.querette@gmail.com>
2024-06-24 12:58:26 -0300

    added servlet files

---
+++ src/main/java/petshop/TutoriaServlet.java
@@ -0,0 +1,51 @@
+package petshop;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet
+class TutoriaServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("Foi chamado um GET. Essa ação retorna um cadastro.");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("Foi chamado um POST. Essa ação cria um novo cadastro.");
+		
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("Foi chamado um PUT. Essa ação substitui/atualiza um cadastro.");
+		
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print("Foi chamado um DELETE. Essa ação remove um cadastro.");
+		
+	}
+
+}
